9ecc5472972d79303579415f82ec6dadd1d00874
sam <bogi99@gmail.com>
2026-05-03 12:55:21 -0400

    initial commit for the rat is coming sooN

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <style>
+
+        </style>
+    @endif
+</head>
+
+<body class="bg-slate-400">
+    <div class="flex h-screen items-center justify-center">
+        <h1 class="text-6xl font-bold text-gray-800 shadow-lg shadow-blue-600">The Rat is coming!</h1>
+    </div>
+
+
+
+
+</body>
+
+</html>
